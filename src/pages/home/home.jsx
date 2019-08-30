ad4f930eacfa733c48ab108c1e93745915098368
LeoooY <Leoyy3@163.com>
2019-08-30 18:55:38 +0800

    add deploy

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,17 @@
+import './home.less'
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Home</div>
+    )
+  }
+}
+
+export default Home;
